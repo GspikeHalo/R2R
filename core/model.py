a2603a5dacc35be5456d6d065868259e87c2e91e
gspikehalo <2318002579@qq.com>
2025-07-20 23:29:46 -0700

    new Relu

--- core/model.py
+++ core/model.py
@@ -18,7 +18,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from core.wing import FAN
-from core.generator import Conformer
 
 
 class ResBlk(nn.Module):
@@ -98,7 +97,7 @@ class AdainResBlk(nn.Module):
 
     def _shortcut(self, x):
         if self.upsample:
-            x = F.interpolate(x, scale_factor=2, mode='nearest')
+            x = F.interpolate(x, scale_factor=2, mode='bicubic')
         if self.learned_sc:
             x = self.conv1x1(x)
         return x
@@ -107,7 +106,7 @@ class AdainResBlk(nn.Module):
         x = self.norm1(x, s)
         x = self.actv(x)
         if self.upsample:
-            x = F.interpolate(x, scale_factor=2, mode='nearest')
+            x = F.interpolate(x, scale_factor=2, mode='bicubic')
         x = self.conv1(x)
         x = self.norm2(x, s)
         x = self.actv(x)
@@ -186,11 +185,11 @@ class Generator(nn.Module):
             x = block(x, s)
             skip = skips[-idx-1]
             if skip.shape[2:] != x.shape[2:]:
-                skip = F.interpolate(skip, size=x.shape[2:], mode='bilinear', align_corners=False)
+                skip = F.interpolate(skip, size=x.shape[2:], mode='bicubic', align_corners=False)
             x = x + skip
             if masks is not None and x.size(2) in [32, 64, 128]:
                 mask = masks[0] if x.size(2) == 32 else masks[1]
-                mask = F.interpolate(mask, size=x.size(2), mode='bilinear', align_corners=False)
+                mask = F.interpolate(mask, size=x.size(2), mode='bicubic', align_corners=False)
                 x = x + self.hpf(mask * cache[x.size(2)])
 
         return self.to_rgb(x)
@@ -201,20 +200,20 @@ class MappingNetwork(nn.Module):
         super().__init__()
         layers = []
         layers += [nn.Linear(latent_dim, 512)]
-        layers += [nn.ReLU()]
+        layers += [nn.LeakyReLU()]
         for _ in range(3):
             layers += [nn.Linear(512, 512)]
-            layers += [nn.ReLU()]
+            layers += [nn.LeakyReLU()]
         self.shared = nn.Sequential(*layers)
 
         self.unshared = nn.ModuleList()
         for _ in range(num_domains):
             self.unshared += [nn.Sequential(nn.Linear(512, 512),
-                                            nn.ReLU(),
+                                            nn.LeakyReLU(),
                                             nn.Linear(512, 512),
-                                            nn.ReLU(),
+                                            nn.LeakyReLU(),
                                             nn.Linear(512, 512),
-                                            nn.ReLU(),
+                                            nn.LeakyReLU(),
                                             nn.Linear(512, style_dim))]
 
     def forward(self, z, y):

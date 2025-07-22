ccbbf35059c1542fd0981adf472b6c1cde1e5936
gspikehalo <2318002579@qq.com>
2025-07-22 16:51:51 -0700

    cot

--- core/model.py
+++ core/model.py
@@ -78,17 +78,17 @@ class AdaIN(nn.Module):
 
 
 class AdainResBlk(nn.Module):
-    def __init__(self, dim_in, dim_out, style_dim=64, w_hpf=0,
+    def __init__(self, dim_in, dim_out, skip_in, style_dim=64, w_hpf=0,
                  actv=nn.LeakyReLU(0.2), upsample=False):
         super().__init__()
         self.w_hpf = w_hpf
         self.actv = actv
         self.upsample = upsample
         self.learned_sc = dim_in != dim_out
-        self._build_weights(dim_in, dim_out, style_dim)
+        self._build_weights(dim_in, dim_out, skip_in, style_dim)
 
-    def _build_weights(self, dim_in, dim_out, style_dim=64):
-        self.conv1 = nn.Conv2d(dim_in, dim_out, 3, 1, 1)
+    def _build_weights(self, dim_in, dim_out, skip_in, style_dim=64):
+        self.conv1 = nn.Conv2d(dim_in + skip_in, dim_out, 3, 1, 1)
         self.conv2 = nn.Conv2d(dim_out, dim_out, 3, 1, 1)
         self.norm1 = AdaIN(style_dim, dim_in)
         self.norm2 = AdaIN(style_dim, dim_out)
@@ -102,19 +102,21 @@ class AdainResBlk(nn.Module):
             x = self.conv1x1(x)
         return x
 
-    def _residual(self, x, s):
+    def _residual(self, x, s, skip):
         x = self.norm1(x, s)
         x = self.actv(x)
         if self.upsample:
             x = F.interpolate(x, scale_factor=2, mode='bicubic')
+        if skip is not None:
+            x = torch.cat([x, skip], dim=1)
         x = self.conv1(x)
         x = self.norm2(x, s)
         x = self.actv(x)
         x = self.conv2(x)
         return x
 
-    def forward(self, x, s):
-        out = self._residual(x, s)
+    def forward(self, x, s, skip):
+        out = self._residual(x, s, skip)
         if self.w_hpf == 0:
             out = (out + self._shortcut(x)) / math.sqrt(2)
         return out
@@ -155,7 +157,7 @@ class Generator(nn.Module):
             self.encode.append(
                 ResBlk(dim_in, dim_out, normalize=True, downsample=True))
             self.decode.insert(
-                0, AdainResBlk(dim_out, dim_in, style_dim,
+                0, AdainResBlk(dim_out, dim_in, dim_in, style_dim,
                                w_hpf=w_hpf, upsample=True))  # stack-like
             dim_in = dim_out
 
@@ -164,7 +166,7 @@ class Generator(nn.Module):
             self.encode.append(
                 ResBlk(dim_out, dim_out, normalize=True))
             self.decode.insert(
-                0, AdainResBlk(dim_out, dim_out, style_dim, w_hpf=w_hpf))
+                0, AdainResBlk(dim_out, dim_out, 0, style_dim, w_hpf=w_hpf))
 
         if w_hpf > 0:
             device = torch.device(
@@ -182,15 +184,15 @@ class Generator(nn.Module):
             x = block(x)
 
         for idx, block in enumerate(self.decode):
-            x = block(x, s)
-            skip = skips[-idx-1]
-            if skip.shape[2:] != x.shape[2:]:
-                skip = F.interpolate(skip, size=x.shape[2:], mode='bicubic', align_corners=False)
-            x = x + skip
-            if masks is not None and x.size(2) in [32, 64, 128]:
-                mask = masks[0] if x.size(2) == 32 else masks[1]
-                mask = F.interpolate(mask, size=x.size(2), mode='bicubic', align_corners=False)
-                x = x + self.hpf(mask * cache[x.size(2)])
+            if idx != 0 and idx != 1:
+                skip = skips[-idx-1]
+            else:
+                skip = None
+            x = block(x, s, skip)
+            # if masks is not None and x.size(2) in [32, 64, 128]:
+            #     mask = masks[0] if x.size(2) == 32 else masks[1]
+            #     mask = F.interpolate(mask, size=x.size(2), mode='bicubic', align_corners=False)
+            #     x = x + self.hpf(mask * cache[x.size(2)])
 
         return self.to_rgb(x)
 

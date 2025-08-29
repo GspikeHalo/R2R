17cdd12a5d528fa34b08830375a52b0e0c045b66
GspikeHalo <2318002579@qq.com>
2025-08-29 00:24:22 -0400

    remove FBM, retain ID loss

--- core/utils.py
+++ core/utils.py
@@ -33,13 +33,11 @@ def print_network(network, name):
 
 
 def he_init(module):
-    if getattr(module, 'skip_he_init', False):
-        return
     if isinstance(module, nn.Conv2d):
         nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='relu')
         if module.bias is not None:
             nn.init.constant_(module.bias, 0)
-    elif isinstance(module, nn.Linear):
+    if isinstance(module, nn.Linear):
         nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='relu')
         if module.bias is not None:
             nn.init.constant_(module.bias, 0)
